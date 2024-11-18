66bf40ca30bdc0cbe0c702450e8ef3193f62da86
Roi Zerah <roizerah@Rois-MacBook-Pro.local>
2024-11-18 14:57:06 +0200

    first try

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,23 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String input = args[0];
+            int repeat = Integer.parseInt(args[1]);
+            input = input.toUpperCase();
+            String forstring = "AEFHILMNORSX";
+            for(int i = 0; i < input.length(); i++){
+                String indexcheck = "";
+                if(forstring.indexOf(input.charAt(i))== -1){
+                        indexcheck = "a ";
+                }
+                else{
+                        indexcheck = "an";
+                }
+                System.out.println("Give me " + indexcheck + " " + input.charAt(i) +": " + input.charAt(i) + "!");
+            }
+            System.out.println("What does that spell?");
+            for(int i=0;i < repeat; i++){
+            System.out.println(input + "!!!");
+            }
         }
 }
